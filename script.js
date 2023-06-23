1cd8f27d96e2096c33dc6ce612c95b16cf5e8805
psantoso13 <prayogo.santoso@gmail.com>
2023-06-23 17:23:59 +0700

    Update JS, add counter and prompt input count num

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
+let totalCount = prompt('Masukkan jumlah hitungan:')
 let countEl = document.getElementById('count_el')
 let count = 0;
+let error = new Audio('./error.mp3');
+
+function back() {
+    count = 0;
+    countEl.innerText = 0;
+}
+    
 
 function add() {
-    count += 1;
-    countEl.innerText = count;
+    if (count < totalCount) { 
+        count += 1;
+        countEl.innerText = count;
+    } else {
+        error.play()        
+        alert(`jumlah hitungan sudah ${totalCount} kali`)
+        back()
+    }        
 }
 
 function reset() {
+    let konfirmasi = confirm('Mulai dari awal?')
+
+    if(konfirmasi === true){
     count = 0;
     countEl.innerText = 0;
+} else {}
+
+}
+
+function newCount() {
+    let konfirmasi = confirm('Masukkan jumlah hitungan baru?')
+
+    if(konfirmasi === true){
+    count = 0;
+    countEl.innerText = 0;
+    location.reload()
+} else {}
+
 }
